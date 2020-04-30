e3b88d258b8c9767db7f4834bc70c16d32713901
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2020-04-30 11:01:23 +0100

    added differrent functionalities to reminder page
    the calendar is now able to switch between different months
    user can  add different reminders for the same day

--- front-end/reminders.js
+++ front-end/reminders.js
@@ -1,179 +1,21 @@
-//  to hide script contents from old browsers 
-
-//
-
-//  Cookie Functions
-
-//  Written by:  Bill Dortch, hIdaho Design
-
-//  The following functions are released to the public domain.
-
-//
-
-//
-
-// "Internal" function to encode cookie value.  This permits cookies to
-
-// contain whitespace, comma and semicolon characters.
-
-//
-
-function encode (str) {
-  var dest = "";
-  var len = str.length;
-  var index = 0;
-  var code = null;
-  
-  for (var i = 0; i < len; i++) {
-    var ch = str.charAt(i);
-    if (ch == " ") code = "%20";
-    else if (ch == "%") code = "%25";
-    else if (ch == ",") code = "%2C";
-    else if (ch == ";") code = "%3B";
-    else if (ch == "\b") code = "%08";
-    else if (ch == "\t") code = "%09";
-    else if (ch == "\n") code = "%0A";
-    else if (ch == "\f") code = "%0C";
-    else if (ch == "\r") code = "%0D";
-
-    if (code != null) {
-      dest += str.substring(index,i) + code;
-      index = i + 1;
-      code = null;
-    }
-  }
-
-  if (index < len){
-    dest += str.substring(index, len);}
-  return dest;
-}
-
-
-
-//
-
-// "Internal" function to decode cookie values.
-
-//
-
-function decode (str) {
-  var dest = "";
-  var len = str.length;
-  var index = 0;
-  var code = null;
-  var i = 0;
-
-  while (i < len) {
-    i = str.indexOf ("%", i);
-    if (i == -1)break;
-
-    if (index < i)
-      dest += str.substring(index, i);
-    code = str.substring (i+1,i+3);
-    i += 3;
-
-    index = i;
-
-    if (code == "20") dest += " ";
-    else if (code == "25") dest += "%";
-    else if (code == "2C") dest += ",";
-    else if (code == "3B") dest += ";";    
-    else if (code == "08") dest += "\b";
-    else if (code == "09") dest += "\t";
-    else if (code == "0A") dest += "\n";
-    else if (code == "0C") dest += "\f";
-    else if (code == "0D") dest += "\r";
-    else {
-
-      i -= 2;
-      index -= 3;
-    }
-  }        
-
-  if (index < len)
-    dest += str.substring(index, len);
-  return dest;
-}
-
-
-
-//
-
-// "Internal" function to return the decoded value of a cookie
-
-//
-
-function getCookieVal (offset) {
-  var endstr = document.cookie.indexOf (";", offset);
-  if (endstr == -1)
-    endstr = document.cookie.length;
-  return decode(document.cookie.substring(offset, endstr));
-}
-
-
-
-//
-
-//  Function to return the value of the cookie specified by "name".
-
-//    name - String object containing the cookie name.
-
-//
-
-function GetCookie (name) {
-  var arg = name + "=";
-  var alen = arg.length;
-  var clen = document.cookie.length;
-  var i = 0;
-
-  while (i < clen) {
-    var j = i + alen;
-    if (document.cookie.substring(i, j) == arg)
-      return getCookieVal (j);
-    i = document.cookie.indexOf(" ", i) + 1;
-    if (i == 0) break; 
-
-  }
-  return null;
-}
-
-function GetLocalStorage (key) {
-  if (localStorage.getItem(key) != null) 
+function GetLocalStorage(key) {
+  if (localStorage.getItem(key) != null)
     return localStorage.getItem(key);
   return null;
 }
 
-
-
-//
-
-//  Function to create or update a cookie.
-
-//    name - String object object containing the cookie name
-
-//    value - String object containing the cookie value.  May contain
-
-//      any valid sting characters, including whitespace, commas and quotes.
-
-//    expires - Date object containing the expiration data of the cookie,
-
-//      or null to expire the cookie at the end of the current session.
-
-//
-
-function SetCookie (name, value, expires) {
-  document.cookie = name + "=" + encode(value) + ((expires == null) ? "" : ("; expires=" + expires.toGMTString()));
-  console.log('Cookie set: '+name + "=" + encode(value) + ((expires == null) ? "" : ("; expires=" + expires.toGMTString())));
-}
-
-var lastKey;
-function SetLocalStorage (key, value) {
-  lastKey = key;
+function SetLocalStorage(key, value) {
+  const items = {
+    ...localStorage
+  };
+  let increment = Object.keys(items).length;
+  key = increment.toString() + ' ' + key;
   localStorage.setItem(key, value);
 }
 
-
-function createReminderItems (key, value)  {
+function createReminderItems(key, value) {
+  day = key.split(' ')[1]
+  month = key.split(' ')[2]
   var reminderList = document.getElementById("reminderList");
   var newReminderItem = value;
   var li = document.createElement("li");
@@ -182,162 +24,121 @@ function createReminderItems (key, value)  {
   reminderList.appendChild(li);
   var reminderDate = document.createElement('div');
   reminderDate.className = 'float-right';
-  // reminderDate.appendChild(document.createTextNode(key+"th"));
-  ((parseInt(key%10)) === 1)?  reminderDate.appendChild(document.createTextNode(key+"st")):
-  ((parseInt(key%10)) === 2)?  reminderDate.appendChild(document.createTextNode(key+"nd")):
-  ((parseInt(key%10)) === 3)?  reminderDate.appendChild(document.createTextNode(key+"rd")):
-  reminderDate.appendChild(document.createTextNode(key+"th"));
+  ((parseInt(key % 10)) === 1) ? reminderDate.appendChild(document.createTextNode(month + " " + day + "st")):
+    ((parseInt(key % 10)) === 2) ? reminderDate.appendChild(document.createTextNode(month + " " + day + "nd")) :
+    ((parseInt(key % 10)) === 3) ? reminderDate.appendChild(document.createTextNode(month + " " + day + "rd")) :
+    reminderDate.appendChild(document.createTextNode(month + " " + day + "th"));
   li.appendChild(reminderDate);
 }
-  
-function loadListItems () {
-  const items = { ...localStorage };
+
+function loadListItems() {
+  const items = {
+    ...localStorage
+  };
   window.onload = function init() {
-    for (var i=0; i<Object.keys(items).length; i++) {
+    for (var i = 0; i < Object.keys(items).length; i++) {
       console.log(Object.keys(items)[i], Object.values(items)[i]);
       createReminderItems(Object.keys(items)[i], Object.values(items)[i]);
     }
   }
 }
 
-//  Function to delete a cookie. (Sets expiration date to current date/time)
+function arrayOfDaysInMonths(isLeapYear) {
+  this[0] = 31;
+  this[1] = 28;
 
-//    name - String object containing the cookie name
-
-//
-
-function DeleteCookie (name) {
-  var exp = new Date();
-  var cval = GetCookie (name);
-  document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
-}
-//
-
-
-function arrayOfDaysInMonths(isLeapYear)
-{
-   this[0] = 31;
-   this[1] = 28;
-
-   if (isLeapYear)
-   this[1] = 29;
-   this[2] = 31;
-   this[3] = 30;
-   this[4] = 31;
-   this[5] = 30;
-   this[6] = 31; 
-   this[7] = 31;
-   this[8] = 30;
-   this[9] = 31;
-   this[10] = 30;
-   this[11] = 31;
+  if (isLeapYear)
+    this[1] = 29;
+  this[2] = 31;
+  this[3] = 30;
+  this[4] = 31;
+  this[5] = 30;
+  this[6] = 31;
+  this[7] = 31;
+  this[8] = 30;
+  this[9] = 31;
+  this[10] = 30;
+  this[11] = 31;
 }
 
-function daysInMonth(month, year){
+function daysInMonth(month, year) {
+  // do the classic leap year calculation
+  var isLeapYear = (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
+  var monthDays = new arrayOfDaysInMonths(isLeapYear);
 
-// do the classic leap year calculation
-   var isLeapYear = (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
-   var monthDays  = new arrayOfDaysInMonths(isLeapYear);
-
-   return monthDays[month];
+  return monthDays[month];
 }
 
-function calendar()
-{
+today = new Date();
+currentMonth = today.getMonth();
+currentYear = today.getFullYear();
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+showCalendar(currentMonth, currentYear);
+
+function next() {
+  currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+  currentMonth = (currentMonth + 1) % 12;
+  showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+  currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+  currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+  showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
   loadListItems();
-  var monthNames = "JanFebMarAprMayJunJulAugSepOctNovDec";
-  var today      = new Date();
-  var day        = today.getDate();
-  var month      = today.getMonth();
-  var year       = today.getYear() + 1900;
+  var today = new Date();
+  var firstDay = today;
+  firstDay.setDate(1);
 
-// figure out how many days this month will have...
+  var startDay = firstDay.getDay();
 
-   var numDays    = daysInMonth(month, year);
+  // Start the calendar table     
+  calendar = document.getElementById("calendar-body");
+  calendar.innerHTML = "";
+  monthAndYear.innerHTML = months[month] + " " + year;
+  let date = 1;
+  for (let i = 0; i < 6; i++) {
+    // creates a table row
+    let row = document.createElement("tr");
 
-// and go back to the first day of the month...
+    //creating individual cells, filing them up with data.
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < startDay) {
+        cell = document.createElement("td");
+        cellText = document.createTextNode("");
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      } else if (date > daysInMonth(month, year))
+        break;
 
-   var firstDay   = today;
-       firstDay.setDate(1);
-
-// and figure out which day of the week it hits...
-
-   var startDay = firstDay.getDay();
-   var column = 0;
-
-   // Start the calendar table     
-   document.write("<CENTER>");
-   document.write("<TABLE BORDER>");
-   document.write("<TR><TH COLSPAN=7>");
-   document.write(monthNames.substring(3*month, 3*(month + 1)) + " " + year);
-   document.write("</th></tr><TR><TH>Sun</th><TH>Mon</th><TH>Tue</th><TH>Wed</th><TH>Thu</th><TH>Fri</th><TH>Sat</th></tr>");
-
-   // put blank table entries for days of week before beginning of the month
-
-   document.write("<TR>");
-   column = 0;
-   for (i=0; i<startDay; i++)
-   {
-      document.write("<TD>&nbsp;</td>");
-      column++;
-   }
- 
-   for (i=1; i <= numDays; i++)
-   {
-      // Write the day
-      var s = "" + i;
-      if ((GetLocalStorage(i) != null))
-
-        // s = s.fontcolor(document.vlinkColor);
-        s = s.fontcolor("#FF0000");
-      s = s.link("javascript:dayClick(" + i + ")")
-          document.write("<TD>" + s);
-
-      // Check for end of week/row
-      if (++column == 7)
-      {
-         document.write("</tr><TR>"); // start a new row
-         column = 0;
+      else {
+        cell = document.createElement("td");
+        var s = "" + date;
+        if ((GetLocalStorage(date) != null))
+          s = s.fontcolor("#FF0000");
+        s = s.link("javascript:dayClick(" + date + ")")
+        console.log(s);
+        cell.innerHTML = s;
+        for (var child = cell.firstChild; child !== null; child = cell.nextSibling) {
+          cell.appendChild(child);
+          row.appendChild(cell);
+        }
+        date++;
       }
-      document.write("</td>");
-   }
-   document.write("</tr></TABLE>");
-   document.writeln("</CENTER>");
-
+    }
+    calendar.appendChild(row);
+  }
 }
 
-
-
-////////////////////////////
-
-//////// dayClick //////////
-
-////////////////////////////
-
-function dayClick(day)
-{
-        var expdate = new Date ();
-            expdate.setTime (expdate.getTime() + (24 * 60 * 60 * 1000)); // 24 hrs from now
-        // var prefix                = "D";
-        var theStorageItemKey         = day;
-        var theDayclickedReminder = GetLocalStorage(theStorageItemKey);
-    if (theDayclickedReminder != null) {
-        alert("The reminder for day " + day + " is: "  + theDayclickedReminder);
-    } // end if
-        if (confirm("Do you wish to enter a reminder for day " + day + " of this month?"))
-        {
-                reminder = prompt("Enter a reminder for day "+ day + " of this month", theDayclickedReminder);
-        SetLocalStorage (theStorageItemKey, reminder);
-        createReminderItems (theStorageItemKey, reminder);
-    } // end if
+function dayClick(day) {
+  key = day + ' ' + months[currentMonth];
+  var theStorageItemKey = key;
+  var theDayclickedReminder = GetLocalStorage(theStorageItemKey);
+  reminder = prompt("Enter a reminder for day " + day + " of this month", theDayclickedReminder);
+  SetLocalStorage(theStorageItemKey, reminder);
+  createReminderItems(' ' + theStorageItemKey, reminder);
 }
-
-// --> <!-- end hiding contents from old browsers  -->
-// to hide script contents from old browsers
-// Write the intro
-// Write the calendar
-
-calendar();
-
-
-// --> <!-- end hiding contents from old browsers  -->
